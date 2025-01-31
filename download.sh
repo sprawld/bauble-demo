69b9b31a65e7d37390f64ad18cd50835b6f1279f
sprawld <sprawld@gmail.com>
2025-01-31 19:37:10 +0000

    download latest bauble player & compiler

---
+++ download.sh
@@ -0,0 +1,3 @@
+# https://bauble.studio/help/#embedding
+wget https://bauble.studio/dist/compiler.esm.2ad74cddb7ad08d4.js -O src/lib/compiler.js
+wget https://bauble.studio/dist/player.esm.e723978195dfc801.js -O src/lib/player.js
